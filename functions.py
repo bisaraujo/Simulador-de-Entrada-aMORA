94f357908b69fc2b0bfe2f2cd2a9d90ad109b3fa
Gabrielli Araujo <gabisaraujo0707@gmail.com>
2025-08-23 23:31:21 -0300

    base de funcionalidades completa

---
+++ functions.py
@@ -0,0 +1,26 @@
+def valor_entrada(valor_imovel, percentual_entrada):
+    valor_entrada = valor_imovel * (percentual_entrada / 100)
+    return valor_entrada
+
+def valor_a_guardar(valor_imovel):
+    total_a_guardar = valor_imovel * 0.15
+    return total_a_guardar
+
+def parcela_mensal(total_a_guardar, anos_contrato):
+    parcela_mensal = total_a_guardar / (anos_contrato * 12)
+    return parcela_mensal
+
+def parcela_anoN_igpm(parcela_mensal, N):
+    dici_parcelas_igpm = {}
+    for ano in range(N, 0, -1):
+        parcela_anoN_igpm = parcela_mensal * (1 + 0.06)**(ano-1)
+        dici_parcelas_igpm[f'Ano {ano}'] = parcela_anoN_igpm
+    return dici_parcelas_igpm
+
+def parcela_anoN_juros(parcela_mensal, N, taxa_juros):
+    dici_parcelas_juros = {}
+    for ano in range(N, 0, -1):
+        parcela_anoN_juros = parcela_mensal * (1 + taxa_juros/100)**(N-1)
+        dici_parcelas_juros[f'Ano {ano}'] = parcela_anoN_juros
+    return dici_parcelas_juros
+
---
+++ main.py
@@ -0,0 +1,23 @@
+from functions import valor_entrada, valor_a_guardar, parcela_mensal, parcela_anoN_igpm, parcela_anoN_juros
+
+valor_imovel = int(input("Informe o valor do imóvel: "))
+percentual_entrada = int(input("Informe o percentual da entrada: "))
+anos_contrato = int(input("Informe a duração do contrato em anos: "))
+taxa_juros = int(input("Informe a taxa de juros sugerida em porcentagem: "))
+
+valor_entrada = valor_entrada(valor_imovel, percentual_entrada)
+valor_a_guardar = valor_a_guardar(valor_imovel)
+parcela_mensal = parcela_mensal(valor_a_guardar, anos_contrato)
+dici_parcelas_igpm = parcela_anoN_igpm(parcela_mensal, anos_contrato)
+dici_parcelas_juros = parcela_anoN_juros(parcela_mensal, anos_contrato, taxa_juros)
+
+print(f"- Valor da entrada: R${valor_entrada}")
+print(f"Valor a guardar: R${valor_a_guardar}")
+print("Valor mensal pelo IGPM:")
+for ano, valor in dici_parcelas_igpm.items():
+    print(f"{ano}: R${valor}")
+
+print(f"Valor mensal com {taxa_juros} ao ano:")
+for ano, valor in dici_parcelas_juros.items():
+    print(f"{ano}: R${valor}")
+
