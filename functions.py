07ccc0efee2dfa9e3474694bca2288280d6b3a43
Gabrielli Araujo <gabisaraujo0707@gmail.com>
2025-08-23 23:40:44 -0300

    Adicionando valor_mensal_base e corrigindo string

--- functions.py
+++ functions.py
@@ -15,7 +15,10 @@ def parcela_anoN_igpm(parcela_mensal, N):
     for ano in range(N, 0, -1):
         parcela_anoN_igpm = parcela_mensal * (1 + 0.06)**(ano-1)
         dici_parcelas_igpm[f'Ano {ano}'] = parcela_anoN_igpm
-    return dici_parcelas_igpm
+        if ano == 1:
+            valor_mensal_base = parcela_anoN_igpm
+            
+    return dici_parcelas_igpm, valor_mensal_base
 
 def parcela_anoN_juros(parcela_mensal, N, taxa_juros):
     dici_parcelas_juros = {}
@@ -24,3 +27,5 @@ def parcela_anoN_juros(parcela_mensal, N, taxa_juros):
         dici_parcelas_juros[f'Ano {ano}'] = parcela_anoN_juros
     return dici_parcelas_juros
 
+
+    
